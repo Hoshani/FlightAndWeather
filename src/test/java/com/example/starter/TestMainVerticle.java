081c7d6966561e01449f5fbc7c10fdb2c0ef46f1
Hoshani <Hoshani.mohammed@gmail.com>
2021-12-22 15:18:06 +0300

    modified pom file & unit tests

--- src/test/java/com/example/starter/TestMainVerticle.java
+++ src/test/java/com/example/starter/TestMainVerticle.java
@@ -1,35 +1,35 @@
-package com.example.test;
+package com.example.starter;
 
-import com.example.starter.AviationService;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
-import io.vertx.junit5.VertxExtension;
+import io.vertx.ext.unit.junit.RunTestOnContext;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 
-@ExtendWith(VertxExtension.class)
+
 @RunWith(VertxUnitRunner.class)
 public class TestMainVerticle {
 
-//  @Rule
-//  public RunTestOnContext rule = new RunTestOnContext();
-//
-//  @Before
-//  public void deploy_verticle(TestContext testContext) {
-//    Vertx vertx = rule.vertx();
-//    vertx.deployVerticle(new AviationService(), testContext.asyncAssertSuccess());
-//  }
-//
-//  @Test
-//  public void verticle_deployed(TestContext testContext) throws Throwable {
-//    Async async = testContext.async();
-//    async.complete();
-//  }
+  @Rule
+  public RunTestOnContext rule = new RunTestOnContext();
+
+  @Before
+  public void deploy_verticle(TestContext testContext) {
+    Vertx vertx = rule.vertx();
+    vertx.deployVerticle(new AviationService(), testContext.asyncAssertSuccess());
+  }
+
+  @Test
+  public void verticle_deployed(TestContext testContext) throws Throwable {
+    Async async = testContext.async();
+    async.complete();
+  }
 
 
   @Test
